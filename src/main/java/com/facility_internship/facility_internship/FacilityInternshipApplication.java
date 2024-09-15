c47d19f6fdb288ed844beb6980fe56192c7e2c3f
thang7904 <trinhducthang.7904@gmail.com>
2024-09-15 21:56:16 +0700

    first commit

---
+++ src/main/java/com/facility_internship/facility_internship/FacilityInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.facility_internship.facility_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityInternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facility_internship/facility_internship/FacilityInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facility_internship.facility_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
